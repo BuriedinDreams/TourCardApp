4143e937e0f95c59e8e71fcbe92abb2b336ac151
Zack W <zackw0430@gmail.com>
2022-01-16 17:33:36 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+import TourCard from './compnents/TourCard';
+// import Paper from '@mui/material/Paper';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <TourCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/compnents/TourCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Paper from '@mui/material/Paper';
+
+
+const TourCard = () => {
+    return (
+        <Paper elevation={4} >
+            <p>Hello peeps</p>
+        </Paper>
+    )
+};
+
+
+export default TourCard;
+
